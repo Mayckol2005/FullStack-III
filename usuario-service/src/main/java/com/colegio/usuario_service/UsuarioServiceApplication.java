aa1edfea5782183071b8fddc81b689a506b5a20b
Mayckol2005 <may.mardones@duocuc.cl>
2026-04-03 18:21:18 -0300

    Microservicio de usuarios

--- usuario-service/src/main/java/com/colegio/usuario_service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/colegio/usuario_service/UsuarioServiceApplication.java
@@ -1,13 +1,13 @@
-package com.shopsmart.inventory_service;
+package com.colegio.usuario_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
+		SpringApplication.run(UsuarioServiceApplication.class, args);
 	}
 
 }
--- usuario-service/src/test/java/com/colegio/usuario_service/UsuarioServiceApplicationTests.java
+++ usuario-service/src/test/java/com/colegio/usuario_service/UsuarioServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.shopsmart.inventory_service;
+package com.colegio.usuario_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InventoryServiceApplicationTests {
+class UsuarioServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
