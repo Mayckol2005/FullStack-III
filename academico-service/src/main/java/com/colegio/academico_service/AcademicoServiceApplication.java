45793983bf0193001ead9e7a2a469659daa16eca
francisxo999 <fra.verab@duocuc.cl>
2026-04-04 02:57:51 -0300

    Microservicios funcionando con Eureka (academico y usuario registrados)

--- academico-service/src/main/java/com/colegio/academico_service/AcademicoServiceApplication.java
+++ academico-service/src/main/java/com/colegio/academico_service/AcademicoServiceApplication.java
@@ -2,10 +2,8 @@ package com.colegio.academico_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class AcademicoServiceApplication {
 
 	public static void main(String[] args) {
--- usuario-service/src/main/java/com/colegio/usuario_service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/colegio/usuario_service/UsuarioServiceApplication.java
@@ -2,10 +2,8 @@ package com.colegio.usuario_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
