72d9fd1c46b47ec49c733f3c586c15aa4d757926
francisxo999 <fra.verab@duocuc.cl>
2026-04-06 22:18:27 -0400

    fix: configurar skipTests en microservicios, corregir rutas y desactivar seguridad para desarrollo

--- academico-service/src/main/java/com/colegio/academico_service/config/SecurityConfig.java
+++ academico-service/src/main/java/com/colegio/academico_service/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.colegio.usuario_service.config;
+package com.colegio.academico_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
