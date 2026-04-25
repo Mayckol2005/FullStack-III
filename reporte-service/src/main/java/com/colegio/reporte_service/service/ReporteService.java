d6071098016d54ae6fa859f0adba34d3c8484524
francisxo999 <fra.verab@duocuc.cl>
2026-04-25 05:18:03 -0400

    feat: comunicación exitosa entre Reporte y Estudiante vía OpenFeign.

--- reporte-service/src/main/java/com/colegio/reporte_service/service/ReporteService.java
+++ reporte-service/src/main/java/com/colegio/reporte_service/service/ReporteService.java
@@ -1,18 +1,33 @@
 package com.colegio.reporte_service.service;
 
+import com.colegio.reporte_service.client.EstudianteClient;
 import com.colegio.reporte_service.dto.ReporteGeneralDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ReporteService {
 
+    @Autowired
+    private EstudianteClient estudianteClient;
+
     public ReporteGeneralDto obtenerReporteGeneral() {
-        // En el próximo paso conectaremos OpenFeign aquí
+        int totalReal = 0;
+        String mensajeEstado = "Reporte sincronizado vía OpenFeign 🚀";
+
+        try {
+            List<Object> estudiantes = estudianteClient.listarTodos();
+            totalReal = estudiantes.size();
+        } catch (Exception e) {
+            mensajeEstado = "Aviso: No se pudo conectar con el servicio de Estudiantes.";
+        }
+
         return new ReporteGeneralDto(
-                450,
+                totalReal,
                 5.8,
                 "92%",
-                "Arquitectura refactorizada y lista para OpenFeign 🚀"
+                mensajeEstado
         );
     }
 }
